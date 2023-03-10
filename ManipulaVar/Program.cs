c5286d4bdb780103aafa7cc8a978f0adfac333a6
Vasco Caleiro <vcaleiro@gmail.com>
2023-03-10 14:19:52 +0000

    second commit~

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}
