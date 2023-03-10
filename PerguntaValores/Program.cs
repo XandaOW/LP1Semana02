2e94ee2ea50ae38b551caf76cca9c9a16f1bf467
Vasco Caleiro <vcaleiro@gmail.com>
2023-03-10 14:38:23 +0000

    Defined Variables and added Volume

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira altura:");
+            int a = Console.ReadLine();
+            Console.WriteLine("insira raio:");
+            int r = Console.ReadLine();
+            double pi = 3.1415926;
+            double V = pi * (r*r) * a
+        }
+    }
+}
