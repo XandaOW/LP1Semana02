02b856ba12f45b197e9613c360157eb1cbb4b309
Vasco Caleiro <vcaleiro@gmail.com>
2023-03-10 14:47:20 +0000

    Fixed String into Int problem Final Version

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -7,11 +7,18 @@ namespace PerguntaValores
         static void Main(string[] args)
         {
             Console.WriteLine("insira altura:");
-            int a = Console.ReadLine();
+        
+            int a = int.Parse( Console.ReadLine());
             Console.WriteLine("insira raio:");
-            int r = Console.ReadLine();
+            int r = int.Parse( Console.ReadLine());
             double pi = 3.1415926;
-            double V = pi * (r*r) * a
+            double V = pi * (r*r) * a;
+            double Sa = 2*pi*r*(r+a);
+
+
+
+            Console.WriteLine("The Volume is:" + V);
+            Console.WriteLine("The Area is:" + Sa);
         }
     }
 }
